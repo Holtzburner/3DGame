15d8c894e93a6c18d1208f3c9812ea2b8b0c7926
Major Holtzburner <mihail.tarakanov0403@gmail.com>
2026-04-30 16:15:40 +0300

    Add player character, input mapping, and controller
    Introduce a playable CharacterBody3D with a new player.gd script implementing movement, jumping, gravity, and mouse look (sensitivity, jump velocity, speed constants). Add input actions to project.godot (move directions, jump, UI navigation) and wire the Player scene into Main.tscn as an instance with the Camera set current. Minor scene updates: Player node type changed to CharacterBody3D and Head node renamed/cleaned up. Also add the player.gd UID resource file.

---
+++ scenes/player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.003
+
+@onready var head = $Head
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * SENSITIVITY)
+		head.rotate_x(-event.relative.y * SENSITIVITY)
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
